6cad52160ef17e21bb1497f1c2e14a81b932f887
PerezCasal <perezcasal91@gmail.com>
2022-11-24 23:39:13 -0500

    Updating the Task from Theme 2

--- basic-java/src/homework/task2/Theme2Main.java
+++ basic-java/src/homework/task2/Theme2Main.java
@@ -3,11 +3,11 @@ package homework.task2;
 public class Theme2Main {
     public static void main(String[] args) {
         double price = 10.5d;
-        float ivaPercent = 16f;
+        double ivaPercent = 16d;
         double resultPrice = getPrice(price, ivaPercent);
         System.out.println(resultPrice);
     }
-    private static double getPrice(double price, float ivaPercent) {
+    private static double getPrice(double price, double ivaPercent) {
         double iva =  (ivaPercent / 100) * price;
         return iva + price;
     }
