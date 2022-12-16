4a6a0b5f59aeb29eb88571606dd895aaf8ce8af8
PerezCasal <perezcasal91@gmail.com>
2022-12-16 18:33:56 -0500

    Answering the Homework of the Advanced Java Exercise 6,7,8

--- advanced-java/design-patterns/src/homework/Task678/exe1/PatternsAndUseCases.java
+++ advanced-java/design-patterns/src/homework/Task678/exe1/PatternsAndUseCases.java
@@ -3,39 +3,39 @@ package homework.Task678.exe1;
 public class PatternsAndUseCases {
 /**
  * Design Patterns Characteristics
- * 1-	A problem that is common for many developers.
- * 2-	Find a strategy for solve the problem and a way to everyone can understand and use.
- * 3-	Like refractoring.guru describe the patterns:
- * -	Design patterns are typical solutions to commonly occurring problems in software design. They are like pre-made
+ * 1- A problem that is common for many developers.
+ * 2- Find a strategy for solve the problem and a way to everyone can understand and use.
+ * 3- Like refractoring.guru describe the patterns:
+ *    - Design patterns are typical solutions to commonly occurring problems in software design. They are like pre-made
  *      blueprints that you can customize to solve a recurring design problem in your code.
- * -	They have a:
- * •	Intent: They show the problem and the way to solve it.
- * •	 Motivation: The problem and the solution are explained in more detail.
- * •	Structure: How they are going to do it and how they are going to compose it.
- * •	Code Example: They show a concrete way of solving the problem.
+ *    - They have a:
+ *      • Intent: They show the problem and the way to solve it.
+ *      • Motivation: The problem and the solution are explained in more detail.
+ *      • Structure: How they are going to do it and how they are going to compose it.
+ *      • Code Example: They show a concrete way of solving the problem.
  * Use Case for Patterns
- * 1-	Creational
- * -	Singleton
- * •	To establish the connection with a database, after establishing the first time we can use it without having to
- *      create it again
- * -	Factory
- * •	To change the language of a site, we make a Factory class that, depending on the language we select, gives us
- *      one or the other.
- * -	Builder
- * •	To build an object without the need to build it completely in the first call, but to do it step by step
- * -	Prototype
- * •	If we want to make a copy of an object, we make a prototype object and then we just call its clone
- * 2-	Structural
- * -	Decorator
- * •	We have a site where we want to send a notification to users in several ways.
- * -	Adapter
- * •	We have a class that only receives data in JSON format but our data comes in XML, so we make an adapter that
- *      allows communication between classes
- * -	Façade
- * •	We have a request from a client but to solve that request we must use several classes, which is not very
- *      understandable on the part of the client, to solve it we create a Facade that performs all the operations
- *      pertinent to the request by the client, for example, we have a library and we give the only methods to use it,
- *      all the others classes are behind are hidden
+ * 1- Creationals
+ *    - Singleton
+ *      • To establish the connection with a database, after establishing the first time we can use it without having to
+ *        create it again
+ *    - Factory
+ *      • To change the language of a site, we make a Factory class that, depending on the language we select, gives us
+ *        one or the other.
+ *    - Builder
+ *      • To build an object without the need to build it completely in the first call, but to do it step by step
+ *    - Prototype
+ *      • If we want to make a copy of an object, we make a prototype object and then we just call its clone
+ * 2- Structural
+ *    -	Decorator
+ *      • We have a site where we want to send a notification to users in several ways.
+ *    -	Adapter
+ *      • We have a class that only receives data in JSON format but our data comes in XML, so we make an adapter that
+ *        allows communication between classes
+ *    -	Façade
+ *      • We have a request from a client but to solve that request we must use several classes, which is not very
+ *        understandable on the part of the client, to solve it we create a Facade that performs all the operations
+ *        pertinent to the request by the client, for example, we have a library and we give the only methods to use it,
+ *        all the others classes are behind are hidden
  *
  * **/
 }
