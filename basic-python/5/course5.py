8d9157081123856efdeee4b81152fa17284a4e18
PerezCasal <perezcasal91@gmail.com>
2023-03-01 12:53:17 -0500

    Modified the result and doing it by functions

--- basic-python/5/course5.py
+++ basic-python/5/course5.py
@@ -1,13 +1,21 @@
-print('Options:')
-print(' 1 = Verify is a year is leap on no')
-print(' 2 = Exit')
-print()
-option = input('Introduce an option: ')
-while (option != '2'):
+def verifyLeapYear():
     year = int(input('Introduce the year: '))
     div = year / 100
     if (year % 4 == 0 and div != int(div)):
         print('The year is a leap year')
     else:
         print('The year is not a leap year')
+
+
+def leapYear():
+    print('Options:')
+    print(' 1 = Verify is a year is leap on no')
+    print(' 2 = Exit')
+    print()
     option = input('Introduce an option: ')
+    while (option != '2'):
+        verifyLeapYear()
+        option = input('Introduce an option: ')
+
+
+leapYear()
