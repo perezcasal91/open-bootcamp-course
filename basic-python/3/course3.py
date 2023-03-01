321b13cbb49bbf2ebb36ccc0609c5f436adfbb35
PerezCasal <perezcasal91@gmail.com>
2023-03-01 09:32:34 -0500

    Init with Python, types and some objects methods

---
+++ basic-python/3/course3.py
@@ -0,0 +1,11 @@
+import math
+
+print('Calculate Body Mass Indez')
+print()
+weight = float(input('Please enter your weight in Kg:'))
+height = float(input('Please enter your height in Ms:'))
+
+bodyMassIndex = weight / (height ** 2)
+
+print()
+print('Your BMI is:' + str(math.ceil(bodyMassIndex)))
---
+++ basic-python/3/objects.py
@@ -0,0 +1,89 @@
+# Primitive
+# Int
+numberA = 2
+numberB = 3
+# Operators
+# Sum
+numberA + numberB
+# Substract
+numberA - numberB
+# Multiply
+numberA * numberB
+# Division
+numberA // numberB
+# Rest
+numberA % numberB
+# Pow
+numberA ** numberB
+# Rotate to rigth
+numberA >> numberB
+# Rotate to left
+numberA << numberB
+# Assignation
+numberA += numberB
+numberA -= numberB
+numberA *= numberB
+numberA //= numberB
+numberA **= numberB
+numberA &= numberB
+numberA |= numberB
+numberA ^= numberB
+numberA >>= numberB
+numberA <<= numberB
+# Compare
+numberA == numberB
+numberA >= numberB
+numberA <= numberB
+numberA != numberB
+# Identity
+numberA is numberB
+numberA is not numberB
+# Membership
+numberA in numberB
+numberA not in numberB
+# Bit
+numberA & numberB
+numberA | numberB
+numberA ^ numberB
+numberA << numberB
+numberA >> numberB
+# String
+text = 'hi'
+text.index('h')
+text.join('world')
+text.capitalize()
+text.title()
+text.lower()
+text.upper()
+text.replace('hi', 'hello')
+text.find('hello')
+text.split()
+# Bool
+bool = True
+# Or
+bool = False
+# Float
+float = 5.5
+# Tuple
+tuple = (1, 2, 3)
+# Or
+tuple = (numberA, text, numberB)
+# List
+emptyList = []
+list = ['a', 'b', 'c']
+list.append('d')
+list.remove('b')
+# Dictionary
+dictionary = {'1': 'firts', '2': 'second'}
+dictionary['1']
+# Sets
+setA = {0, 2, 4, 6, 8}
+setB = {1, 2, 5, 4, 9}
+# Sets Join
+setA | setB
+# Sets Interception
+setA & setB
+# Sets Difference
+setA ^ setB
+# Sets Symetric Difference
+setA - setB
